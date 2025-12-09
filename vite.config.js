bfde52ed99b7038d5aa50e486bc0bf45b31c3178
TRUMPIE ($TRMP) <anjasnuralamsyah13@gmail.com>
2025-12-10 01:28:31 +0700

    Remove unnecessary imports in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: "/granny-coin-site/"  // penting untuk GitHub Pages
+  base: "/granny-coin-site/"
 })
